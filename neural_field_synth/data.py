e00ce7402813e5450dd11a0f2c4f5a9debfd5dd5
Ben Hayes <b.j.hayes@qmul.ac.uk>
2021-12-18 10:45:18 +0000

    Tidy up neural fields

---
+++ neural_field_synth/data.py
@@ -0,0 +1,13 @@
+from scipy.io import wavfile
+import torch
+
+
+class NSynthDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
