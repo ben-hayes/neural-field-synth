5eb124e387f06bceb904ae2f6db7abb8e9f84a27
Ben Hayes <b.j.hayes@qmul.ac.uk>
2021-12-18 12:00:23 +0000

    Update FIR synth

--- neural_field_synth/signal.py
+++ neural_field_synth/signal.py
@@ -17,21 +17,22 @@ class FIRNoiseSynth(nn.Module):
         self.ir_length = ir_length
         self.window_length = window_length
         self.hop_length = hop_length
-        self.register_buffer("window", window_fn(window_length))
+        self.register_buffer("window", window_fn(ir_length))
 
     def forward(self, H_re: torch.tensor) -> torch.tensor:
+        # H_re: [time, batch, feat]
         H_im = torch.zeros_like(H_re)
         H_z = torch.complex(H_re, H_im)
 
-        h = torch.fft.irfft(H_z.transpose(1, 2))
+        h = torch.fft.irfft(H_z)
         h = h.roll(self.ir_length // 2, -1)
-        h = F.pad(h, (0, self.window_length - self.ir_length))
         h = h * self.window.view(1, 1, -1)
+        h = F.pad(h, (0, self.window_length - self.ir_length))
         H = torch.fft.rfft(h)
 
-        noise = torch.rand(self.hop_length * H_re.shape[-1] - 1, device=H_re.device)
+        noise = torch.rand(self.hop_length * H_re.shape[0] - 1, device=H_re.device)
         X = torch.stft(noise, self.window_length, self.hop_length, return_complex=True)
         X = X.unsqueeze(0)
-        Y = X * H.transpose(1, 2)
+        Y = X * H.permute(1, 2, 0)
         y = torch.istft(Y, self.window_length, self.hop_length, center=False)
-        return y.unsqueeze(1)[:, :, : H_re.shape[-1] * self.hop_length]
+        return y.t()[: H_re.shape[0] * self.hop_length]
