c314df7a5f744289cd6c8087348012fe4e376da1
Ben Hayes <b.j.hayes@qmul.ac.uk>
2021-12-18 09:55:49 +0000

    Make shell package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="neural field synth",
+    version="0.0.1",
+    description="A differentiable synthesiser driven by neural fields built for NASH 2021",
+    author="Ben Hayes",
+    author_email="b.j.hayes@qmul.ac.uk",
+    url="https://github.com/ben-hayes/neural-field-synth",
+    packages=["neural_field_synth"],
+)
